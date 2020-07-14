b0f46a4b1ce1c423a49af02c46aa41734b9c06e4
Marian <marian.h.rempola@gmail.com>
2020-07-14 13:11:20 -0400

    Initial Commit

---
+++ Fruits of SpiritTests/Fruits_of_SpiritTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fruits_of_SpiritTests.swift
+//  Fruits of SpiritTests
+//
+//  Created by Marian Rempola on 7/14/20.
+//  Copyright © 2020 Klossified Koders. All rights reserved.
+//
+
+import XCTest
+@testable import Fruits_of_Spirit
+
+class Fruits_of_SpiritTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
